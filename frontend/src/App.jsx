a57670fc987704a76ca7d143bfeb20e58a174baf
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2023-11-23 01:16:32 +0100

    create router and main pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,37 @@
+import RootLayout from '../layouts/RootLayout'
+import Login from '../pages/Login'
+import NotFound from '../pages/NotFound'
+import Notes from '../pages/Notes'
+import Profile from '../pages/Profile'
 import './App.scss'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout/>,
+    errorElement: <NotFound/>,
+    children: [
+      {
+        index: true,
+        element: <Notes/>,
+      },
+      {
+        path: 'profile',
+        element: <Profile/>
+      }
+    ]
+  },
+  {
+    path: 'login',
+    element: <Login/>
+  }
+])
 
 function App() {
 
   return (
-    <>
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
